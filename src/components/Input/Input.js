fbdfcf7b0ca4652e617d7c9a89c7834884e3c7cf
Vasily-Konnov <vasiliy.konnov@gmail.com>
2019-10-23 03:47:56 +0400

    restored validation for input #10

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,7 +3,9 @@ import classes from "./Input.module.css";
 import cx from "classnames";
 
 function Input(props) {
-  let classNames = cx(props.className, classes["input"]);
+  let classNames = cx(props.className, classes["input"], {
+    [classes["input__invalid"]]: !props.valid
+  });
 
   return <input {...props} className={classNames} />;
 }
