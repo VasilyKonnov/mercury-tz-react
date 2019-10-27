87d9c481ad5cc6b8255fa1abdcdff457d955fe94
Vasily-Konnov <vasiliy.konnov@gmail.com>
2019-10-27 05:51:25 +0400

    fix  cx from classnames #8

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,12 +1,12 @@
 import React from "react";
 import classes from "./Button.module.css";
-import cx from "classnames";
+import classNames from "classnames";
 
 function Button(props) {
-  let classNames = cx(props.className, classes["button"]);
+  let buttonClassNames = classNames(props.className, classes["button"]);
 
   return (
-    <button {...props} className={classNames}>
+    <button {...props} className={buttonClassNames}>
       {props.children}
     </button>
   );
